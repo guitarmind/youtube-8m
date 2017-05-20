4e69b47e8fc7bbdb16c4d6b297994acb9005ed0b
Mark Peng <markpeng@exosite.com>
2017-05-20 23:43:46 +0800

    Updated model_utils.

--- model_utils.py
+++ model_utils.py
@@ -20,6 +20,29 @@ from tensorflow import logging
 from tensorflow import flags
 import tensorflow.contrib.slim as slim
 
+def get_avg_pooled(model_input, num_frames):
+    num_frames = tf.cast(tf.expand_dims(num_frames, 1), tf.float32)
+    feature_size = model_input.get_shape().as_list()[2]
+
+    denominators = tf.reshape(
+        tf.tile(num_frames, [1, feature_size]), [-1, feature_size])
+    return tf.reduce_sum(model_input,
+                            axis=[1]) / denominators
+
+def get_standard_dev_and_l2(model_input, num_frames, avg_pooled):
+    term1 = get_avg_pooled(tf.pow(model_input, 2.0), num_frames)
+    term2 = tf.pow(avg_pooled, 2.0)
+    l2 = tf.pow(term1, 0.5)
+    return tf.concat([tf.pow(term1 - term2, 0.5), l2], 1)
+
+def get_min_max_pooled(model_input, num_frames):
+    mask = tf.to_float(tf.equal(model_input, 0.0))
+    min_mask = mask * 1e30
+    max_mask = mask * -1e30
+    min_pooled = tf.reduce_min(min_mask + model_input, axis=[1])
+    max_pooled = tf.reduce_max(max_mask + model_input, axis=[1])
+    return tf.concat([min_pooled, max_pooled], 1)
+
 def SampleRandomSequence(model_input, num_frames, num_samples):
   """Samples a random sequence of frames of size num_samples.
 
@@ -93,3 +116,86 @@ def FramePooling(frames, method, **unused_params):
     return tf.reshape(frames, [-1, feature_size])
   else:
     raise ValueError("Unrecognized pooling method: %s" % method)
+
+def make_fully_connected_net(input_, sizes, vocab_size,
+    l2_penalty, batch_norm=False):
+    layers = []
+    normalizer = slim.batch_norm if batch_norm else None
+    for size in sizes:
+        prev = layers[-1] if len(layers) else input_
+        layers.append(
+            slim.fully_connected(
+                prev,
+                size,
+                weights_regularizer=slim.l2_regularizer(l2_penalty),
+                normalizer_fn=normalizer
+            )
+        )
+    return slim.fully_connected(
+        layers[-1] if len(layers) else input_,
+        vocab_size,
+        activation_fn=tf.nn.sigmoid,
+        weights_regularizer=slim.l2_regularizer(l2_penalty)
+    )
+
+def make_fcnet_with_skips(input_, sizes, skip_conns, vocab_size, l2_penalty):
+    """
+    Adds skip connections between layers based on the parameter `skip_conns`,
+    which should be a list of pairs. A connection will be added from the
+    output of the source layer to the output of the target layer. The outputs
+    will be summed before passing to the next layer. The input will be index 0
+    and the layers start from 1.
+
+    If there is a mismatch between the output size of the source and target
+    layers, a linear projection layer will be added. Batch normalization is
+    also enabled for the layers.
+    """
+    skips = {}
+    for (s, e) in skip_conns:
+        assert s < e, "Connections should be feedforward"
+        if e in skips:
+            skips[e].add(s)
+        else:
+            skips[e] = set([s])
+    input_size = input_.get_shape().as_list()[1]
+    layers = []
+
+    for i, size in enumerate(sizes):
+        prev = layers[-1] if len(layers) else input_
+        layers.append(
+            slim.fully_connected(
+                prev,
+                size,
+                weights_regularizer=slim.l2_regularizer(l2_penalty),
+                normalizer_fn=slim.batch_norm
+            )
+        )
+
+        # Check if there is an incoming connection
+        if (i+1) in skips and len(skips[i+1]):
+            for source in skips[i+1]:
+                source_layer = input_ if source == 0 else layers[source-1]
+                source_size = input_size if source == 0 else sizes[source-1]
+                if source_size == size:
+                    layers[-1] = tf.add(layers[-1], source_layer)
+                else:
+                    projection = slim.fully_connected(source_layer, size,
+                        activation_fn=None) # linear layer
+                    layers[-1] = tf.add(layers[-1], projection)
+
+
+    return slim.fully_connected(
+        layers[-1] if len(layers) else input_,
+        vocab_size,
+        activation_fn=tf.nn.sigmoid,
+        weights_regularizer=slim.l2_regularizer(l2_penalty)
+    )
+
+
+def make_conv_relu_pool(input_, conv_size, pool_size, num_channels,
+    batch_norm=False):
+    normalizer = slim.batch_norm if batch_norm else None
+    conv = slim.conv2d(input_, num_channels, [conv_size, 1],
+        normalizer_fn=normalizer)
+    return tf.nn.max_pool(
+        conv, [1, pool_size, 1, 1], [1, pool_size, 1, 1], 'SAME')
